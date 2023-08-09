00d6fca82d6d50611a30f2e2ef538e6f9ccfcd44
Chathurika Gajanayaka <chathurikagajanayaka@chathurikas-air.home>
2023-08-09 04:04:25 +0200

    list categories

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
+#   Your code goes here.
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('budget_me_creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('budget_me')
+
+category = SHEET.worksheet('Category')
+
+data = category.get_all_values()
+print(data)
+
+
+
+
+
+
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
